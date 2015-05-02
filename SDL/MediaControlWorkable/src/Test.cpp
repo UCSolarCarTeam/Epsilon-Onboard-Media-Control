0c0b224a54b2ad8fa0c1a28adcb9748b9b19420d
Byron Seto <byronseto@hotmail.com>
2015-05-02 10:31:49 -0600

    removed another thing

--- SDL/MediaControlWorkable/src/Test.cpp
+++ SDL/MediaControlWorkable/src/Test.cpp
@@ -208,7 +208,7 @@ int gpsWorker(void* data)
 int show_Camera(IplImage* img){	
 	//SDL_RenderClear(renderer);
 	
-	if(updatedImage1 == true){
+	//if(updatedImage1 == true){
 
 		SDL_Surface* surface = SDL_CreateRGBSurfaceFrom((void*)img->imageData,
 			img->width,
@@ -228,8 +228,8 @@ int show_Camera(IplImage* img){
 		SDL_RenderCopyEx(renderer, texture, NULL, &videoRect ,0, NULL, flip);
 		SDL_DestroyTexture(texture);
 		return 1;
-	}
-		return 0;
+	//}
+		// return 0;
 
 }
 
