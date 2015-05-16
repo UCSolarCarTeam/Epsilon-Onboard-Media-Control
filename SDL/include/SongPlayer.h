4c5151ea6b0a9c139c75534dec5e0b9238f93850
Byron Seto <byronseto@hotmail.com>
2015-05-16 14:53:47 -0600

    Cleaning up SongPlayer.h

--- SDL/include/SongPlayer.h
+++ SDL/include/SongPlayer.h
@@ -3,24 +3,48 @@
 #include <ao/ao.h>
 #include <mpg123.h>
 #include <string>
+#include "SongLoader.h"
+
+#define BITS 8
 
 class SongPlayer{
 
+/*****************************************************************************
+Public Functions
+*/
 	public:
 	SongPlayer();
-	int load(std::string songName);
+
 	int play();
 	int back();
 	int next(); 
 	int pause();
+
 	int getCurrentTime();
 	int getSongLength();
+
 	~SongPlayer();
 
-	private:
-	int freeMusic();
-	int SongThread();
 
+/*****************************************************************************
+Private Functions
+*/
+	private:
+	unsigned char *songBuffer;
+	bool playing;
+	SongLoader loader;
+
+	/*For the time bar*/
+
+
+	int load(std::string songName);
+	int freeMusic();
+
+	/*Functions to change 'bool playing' to true or false*/
+	void start();
+	void stop();
+
+	int songThread();
 };
 
 #endif /* SONGPLAYER_H */
