29515eee2eb3446e3d15d1785c86d880bd82ea3d
Hansel Qiu <Hansel@Hansels-MacBook-Air.local>
2015-05-16 15:04:17 -0600

    SongLoader Working

--- SDL/src/SongLoader.cpp
+++ SDL/src/SongLoader.cpp
@@ -17,7 +17,7 @@
 SongLoader::SongLoader()
 {
     counter=0;
-    song=0;
+    song=-1;
     readSongNames();
 }
 
@@ -27,16 +27,17 @@ int SongLoader::readSongNames()
     DIR *dir;
     struct dirent *ent;
     // Change string to directory with songs in it
-    if ((dir = opendir("ExampleMusic")) != NULL)
+    if ((dir = opendir("SongLibrary")) != NULL)
     {
         while ((ent = readdir(dir)) != NULL)
         {
             
+            if (ent->d_name[0] != '.')
             {
                 container.push_back(ent->d_name);
                 counter++;
-                
-            }
+                }
+            
         }
     }
     
@@ -49,22 +50,32 @@ int SongLoader::shuffleSongNames()
     std::random_device rd;
     std::mt19937 g(rd());
     std::shuffle ( container.begin(), container.end(), g );
-    song=0;
+    song=-1;
     return 0;
 }
 
 
 std::string SongLoader::nextSong()
 {
-    CurrentSong=container[song];
     song++;
-    if(song>=counter)
+    CurrentSong=container[song];
+    if(song>=(counter-1))
         shuffleSongNames();
     
     return CurrentSong;
 }
 
+std::string SongLoader::previousSong()
+{
+    if(song<=0)
+    {
+        song=counter;
+    }
+    song--;
+    CurrentSong=container[song];
 
+    return CurrentSong;
+}
 
 
 
