efc8a5e3f30f9a64c55450a65c6d1bf2fb21bf3b
Hansel Qiu <Hansel@Hansels-MacBook-Air.local>
2015-05-16 11:43:58 -0600

    Added to songloader

--- SDL/src/SongLoader.cpp
+++ SDL/src/SongLoader.cpp
@@ -1,30 +1,84 @@
 #include "SongLoader.h"
-#include <stdlib.h>
-#include <dirent.h>
-#include <string.h>
+
+int main()
+{
+
+SongLoader test;
+test.readSongNames();
+test.shuffleSongNames();
+std::cout<<test.nextSong()<<std::endl;
+std::cout<<test.nextSong()<<std::endl;
+std::cout<<test.nextSong()<<std::endl;
+
+return 0;
+
+}
 
 SongLoader::SongLoader()
 {
+	counter=0;
+	song=0;
 	
 }
 
-int SongLoader::readSongNames(std::string container[])
+int SongLoader::readSongNames()
 {
-	int count = 0;
+	
 	DIR *dir;
 	struct dirent *ent;
 	// Change string to directory with songs in it
-	if ((dir = opendir("/home/mario/Documents/solarCarGit/onboardmediacontrol/SDL/assets/")) != NULL)
+	if ((dir = opendir("ExampleMusic")) != NULL)
 	{
 		while ((ent = readdir(dir)) != NULL)
 		{
-			if (strstr(ent->d_name, ".ogg") != NULL)
+			
 			{
-				container[count] = ent->d_name;
-				count++;
+			container[counter]=ent->d_name;
+			counter++;
+			
 			}
 		}
 	}
-	printf("%d\n", count);
-	return count;
+
+	return 0;
 }
+
+int SongLoader::shuffleSongNames()
+{
+
+	std::random_device rd;
+    std::mt19937 g(rd());
+	std::shuffle ( container, container+counter, g );
+	song=0;
+	return 0;
+}
+
+int SongLoader::sortSongNames()
+{
+
+
+	
+}
+
+std::string SongLoader::nextSong()
+{
+CurrentSong=container[song];
+song++;
+if(song>=counter)
+shuffleSongNames();
+
+return CurrentSong;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
