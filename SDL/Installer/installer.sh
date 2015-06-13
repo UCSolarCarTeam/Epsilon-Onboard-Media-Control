b75b35582c80173029631e3d8f8c8b84e926890c
byron seto <byronseto@hotmail.com>
2015-06-13 17:35:11 +0000

    Changed the installer.sh

--- SDL/Installer/installer.sh
+++ SDL/Installer/installer.sh
@@ -2,6 +2,15 @@
 
 echo "This script will download and install all dependencies for the Raspberry Pi running Raspbian (Debian)"
 
+USER=`whoami`
+if [ $USER = "root" ]
+	then
+	echo "Running as ROOT"
+else
+	echo "***********YOU ARE NOT ROOT************" 
+	exit 0
+fi
+
 #OpenCV	(grabbing camera frames)
 #ao.h 	(playing music)
 #mpg123 (loading music)
@@ -13,28 +22,31 @@ apt-get install libmpg123-dev
 
 #SDL stuff
 
+#removes all old folders of SDL2
+ls | grep SDL2 | sudo xargs rm -r
+
 #SDL2.0
 wget https://www.libsdl.org/release/SDL2-2.0.3.tar.gz
 tar -xzvf SDL2-2.0.3.tar.gz
 cd SDL2-2.0.3
-./configure 
-make -j4
-make install -j4
+./configure --host=armv71-raspberry-linux-gnueabihf
+make -j4 
+make install -j4 
 
 #SDL2_ttf
-wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz
-tar -xzvf SDL_ttf-2.0.12.tar.gz
+wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.tar.gz 
+tar -xzvf SDL2_ttf-2.0.12.tar.gz 
 cd SDL2_ttf-2.0.12 
-./configure
-make -j4
-make install -j4
+./configure 
+make -j4 
+make install -j4 
 
 #SDL2_image
 wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.0.tar.gz
 tar -xzvf SDL2_image-2.0.0.tar.gz
 cd SDL2_image-2.0.0 
 ./configure
-make -j4
-make install -j4ls
+make -j4 
+make install -j4 
 
 
