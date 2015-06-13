a20ed441df38a55ba3cc00496a45c11ac9e0fc3d
Byron Seto <bseto@ucalgary.ca>
2015-06-13 21:52:36 +0000

    AutoBootSetup.sh edited online with Bitbucket

--- SDL/Installer/AutoBootSetup.sh
+++ SDL/Installer/AutoBootSetup.sh
@@ -11,19 +11,17 @@ fi
 
 #Sets up Autoboot
 
-if [ $HOME = "/home/pi" ]
-then
+
 echo "You are running on the pi, we will set up Auto Launch"
 cd ..
 FILECONTENTS=`grep -Eo "onboardmediacontrol" /etc/xdg/lxsession/LXDE-pi/autostart`
 echo "filecontents = $FILECONTENTS"
-        if [ -z $FILECONTENTS ]
-        then
-                echo "Setting up AutoLaunch"
-                sudo sh -c 'CAMERAPATH=`pwd`; echo "@$CAMERAPATH/main" >> /etc/xdg/lxsession/LXDE-pi/autostart'
-        else
-                echo "Autolaunch already set up!"
-        fi
+if [ -z $FILECONTENTS ]
+then
+        echo "Setting up AutoLaunch"
+        sudo sh -c 'CAMERAPATH=`pwd`; echo "@$CAMERAPATH/main" >> /etc/xdg/lxsession/LXDE-pi/autostart'
+else
+        echo "Autolaunch already set up!"
 fi
 
 #Sets up Fastboot
