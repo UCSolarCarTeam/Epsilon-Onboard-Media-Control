78019bc415c37d42d370c7b9261af676b3c1d37b
henrytran28 <henrytran28@hotmail.ca>
2016-10-20 21:58:45 -0600

    formatted all files

--- src/SongLoader.cpp
+++ src/SongLoader.cpp
@@ -3,8 +3,8 @@
 
 SongLoader::SongLoader()
 {
-    counter=0;
-    song=-1;
+    counter = 0;
+    song = -1;
     noLibrary = readSongNames();
     shuffleSongNames();
 }
@@ -16,21 +16,19 @@ int SongLoader::libraryLoad()
 
 int SongLoader::readSongNames()
 {
+    DIR* dir;
+    struct dirent* ent;
 
-    DIR *dir;
-    struct dirent *ent;
     // Change string to directory with songs in it
     if ((dir = opendir("/home/Music")) != NULL)
     {
         while ((ent = readdir(dir)) != NULL)
         {
-
             if (ent->d_name[0] != '.')
             {
                 container.push_back(ent->d_name);
                 counter++;
             }
-
         }
     }
     else
@@ -48,16 +46,17 @@ int SongLoader::shuffleSongNames()
         std::random_device rd;
         std::mt19937 g(rd());
         std::shuffle ( container.begin(), container.end(), g );
-        song=-1;
+        song = -1;
         return 0;
     }
+
     return 1;
 }
 
 
 std::string SongLoader::nextSong()
 {
-    if(song>=(counter-1))
+    if (song >= (counter - 1))
     {
         shuffleSongNames();
         song = 0;
@@ -67,37 +66,49 @@ std::string SongLoader::nextSong()
         song++;
     }
 
-    CurrentSong=container[song];
+    CurrentSong = container[song];
 
     if (counter == 0)
+    {
         return "";
+    }
+
     return ("/home/Music/" + CurrentSong);
 }
 
 std::string SongLoader::previousSong()
 {
-    if(song<=0)
+    if (song <= 0)
     {
-        song=counter-1;
+        song = counter - 1;
     }
     else
     {
         song--;
     }
 
-    CurrentSong=container[song];
+    CurrentSong = container[song];
 
     if (counter == 0)
+    {
         return "";
+    }
+
     return ("/home/Music/" + CurrentSong);
 }
 
 std::string SongLoader::currentSong()
 {
     if (song == -1)
+    {
         song++;
+    }
+
     if (counter == 0)
+    {
         return "";
+    }
+
     return ("/home/Music/" + container[song]);
 }
 
