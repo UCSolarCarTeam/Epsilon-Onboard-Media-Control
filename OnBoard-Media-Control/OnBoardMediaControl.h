37da5b602f77384d08abc77c0ea4113b8edfd271
Ben-Graham <bengraham373@gmail.com>
2017-01-22 18:02:01 -0700

    added OnBoardMediaControl files

---
+++ OnBoard-Media-Control/OnBoardMediaControl.h
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QFileInfo>
+#include <QSettings>
+#include <QIcon>
+#include <QDir>
+
+class OnBoardMediaControl : public QApplication
+{
+    public:
+        OnBoardMediaControl(int &argc, char **argv);
+
+        void associateFileTypes(const QStringList &fileTypes);
+};
