27845ed322806086449993ee6dbed71217c07e98
chordatgh1234 <justinhung11@gmail.com>
2018-02-10 11:40:17 -0700

    got the setup script working for unique albums in DownloadSongsAndAlbums.sh

--- DataLayer/SongLibrary/Albumcheck.sh
+++ DataLayer/SongLibrary/Albumcheck.sh
@@ -1,64 +1,53 @@
 #!/usr/bin/env bash
 
 song=(1-06+Animal 1-11+Armageddon+It 1-14+Hysteria 01DangerZone 01TakeOnMe 02PumpedUpKicks)
+uniquealbumlist=()
 albumlist=()
-#for i in "${song[@]}"; do
-#	wget https://s3-us-west-2.amazonaws.com/ucsolarteam.onboardmedia.songs/$i.mp3
-#done 
-
-#album="$(eyeD3 ${song[0]}.mp3 | grep -o -P '(?<=: ).*(?=year)')"
-#album=${album// /_}
-#album=${album//[[:blank:]]/}
-#album=${album::-4}
-#albumlist+=($album)
 
 album="$(eyeD3 ${song[0]}.mp3 | grep -o -P '(?<=: ).*(?=year)')"
 album=${album//[[:blank:]]/}
 album=${album::-4}
 albumlist+=($album)
+uniquealbumlist+=($album)
+album="../Covers/$album.jpg"
+eyeD3 --write-images=$PWD ${song[0]}.mp3
+mv OTHER.JPG $album
+
 
 i=1
+firstfilecheck=true
 while [ $i -lt ${#song[@]} ]; do 
 	album="$(eyeD3 ${song[$i]}.mp3 | grep -o -P '(?<=: ).*(?=year)')"
 	album=${album//[[:blank:]]/}
 	album=${album::-4}
+	albumlist+=($album)
 	albumcheck=false
 	j=0
-	while [ $j -lt ${#albumlist[@]} ]; do 
-		if [ "$album" == "${albumlist[$j]}" ] ; then
+	while [ $j -lt ${#uniquealbumlist[@]} ]; do 
+		if [ "$album" == "${uniquealbumlist[$j]}" ] ; then
 			albumcheck=true
 		fi
 		let j=j+1
 	done
 	
 	if [ "$albumcheck" = false ] ; then
-		albumlist+=($album)
+		uniquealbumlist+=($album)
+		echo song jpg: ${song[$i]}
+		eyeD3 --write-images=$PWD ${song[$i]}.mp3
+		album="../Covers/$album.jpg"
+		mv OTHER.JPG $album
+		let k=k+1
 	fi
-
 	let i=i+1
 done
 
-echo list contains
+echo uniquealbumlist contains
+printf '%s\n' "${uniquealbumlist[@]}"
+
+echo albumlist contains
 printf '%s\n' "${albumlist[@]}"
 
-#eyeD3 --write-images=$PWD *.mp3
 
-#album="$(eyeD3 ${song[0]}.mp3 | grep -o -P '(?<=: ).*(?=year)')"
-#album=${album// /_}
-#album=${album//[[:blank:]]/}
-#album=${album::-4}
-#album="../Covers/$album.jpg"
-#echo album file is: $album
-
-#mv OTHER.JPG $album 
-
-#i=1
-#while [ $i -lt ${#song[@]} ]; do 
-#	mv OTHER$i.JPG ${song[$i]}.jpg
-#	let i=i+1
-#done
-
-#mogrify -format jpg *.png
 
 
 
