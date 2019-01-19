8347c2e0113816edc9d62e4e56c6ab53b974de27
chordatgh1234 <justinhung11@gmail.com>
2019-01-19 15:36:43 -0700

    removed aws credential

--- DownloadAllSongsAndAlbums.sh
+++ DownloadAllSongsAndAlbums.sh
@@ -4,8 +4,6 @@ mkdir -p $HOME/Music/SongLibrary
 mkdir -p $HOME/Pictures/Covers
 cd $HOME/Music/SongLibrary
 
-export AWS_ACCESS_KEY_ID=AKIAJ4M34KXJBKZK4L5A
-export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-west-2
 export AWS_DEFAULT_OUTPUT=text
 
