c99fadb1fd290f56d7ce9d7c72441fe0e958c253
datlam25 <d4nnyl4m@gmail.com>
2020-05-14 18:52:51 -0600

    Clean up uneeded lines

--- DownloadSongsAndAlbums.sh
+++ DownloadSongsAndAlbums.sh
@@ -4,8 +4,6 @@ mkdir -p $HOME/Music/SongLibrary
 cd $HOME/Music/SongLibrary
 
 song=(1-06+Animal 1-11+Armageddon+It 1-14+Hysteria 1-01+Good+Times+Bad+Times 20+Rock+and+Roll+Is+King)
-uniquealbumlist=()
-albumlist=()
 
 for i in "${song[@]}"; do
 	wget https://s3-us-west-2.amazonaws.com/ucsolarteam.onboardmedia.songs/$i.mp3
