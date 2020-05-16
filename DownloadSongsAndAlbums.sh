08c2f635c62cc2ccda995ecc97459104f4f4dc18
datlam25 <d4nnyl4m@gmail.com>
2020-05-16 10:15:54 -0600

    Stop downloading songs that already exist in the local folder

--- DownloadSongsAndAlbums.sh
+++ DownloadSongsAndAlbums.sh
@@ -6,6 +6,8 @@ cd $HOME/Music/SongLibrary
 song=(1-06+Animal 1-11+Armageddon+It 1-14+Hysteria 1-01+Good+Times+Bad+Times 20+Rock+and+Roll+Is+King)
 
 for i in "${song[@]}"; do
+	if [ ! -f $i.mp3 ]; then
 	wget https://s3-us-west-2.amazonaws.com/ucsolarteam.onboardmedia.songs/$i.mp3
+	fi
 done 
 
